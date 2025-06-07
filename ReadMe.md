6b50ff3865a82b060285a784eaccfbe646507a08
Flo Kleiser <floriankleiser@gmail.com>
2025-06-07 10:51:03 +0200

    made card 3d again

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,13 @@
 # ZHdK Interaction Design FS25
 ___
 
-### Module: Webtechnologies 
-### Student: Florian Kleiser
+### Module: Webtechnologies | Student: Florian Kleiser
+
+### Ideas for API:
+- IMDB: https://imdbapi.dev/ 
+- MK8: https://github.com/samyvera/mk8_node_api
+- Nasa: https://api.nasa.gov/
+- Trivia: https://opentdb.com/api_config.php
+- Cards: https://deckofcardsapi.com/
+- MeteoSchweiz: https://opendatadocs.meteoswiss.ch/
+- Cocktail: https://www.thecocktaildb.com/api.php
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-console.log("...fetching a random cocktail 🍹");
-
 const titleContainer = document.querySelector("[data-js='title']");
 const imgContainer = document.querySelector("[data-js='photo']");
 
+const card = document.querySelector(".card") as HTMLElement;
+let cardBounds = card.getBoundingClientRect() as DOMRect;
+
 if (!titleContainer || !imgContainer) {
     throw new Error("Required DOM elements not found");
+}
+
+if (!card) {
+    throw new Error("Card missing");
 }
 
 fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
@@ -32,3 +37,37 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .catch((error) => {
         console.error("Error:", error);
     });
+
+
+function rotateCard(e:MouseEvent) {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const leftX = mouseX - cardBounds.x;
+    const topY = mouseY - cardBounds.y;
+    const center = {
+        x: leftX - cardBounds.width / 2,
+        y: topY - cardBounds.height / 2,
+    };
+    const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+
+    card.style.transform = `
+        scale3d(1.03, 1.03, 1.03)
+        rotate3d(
+          ${center.y / 100},
+          ${-center.x / 100},
+          0,
+          ${Math.log(distance) * 2}deg
+        )
+      `;
+}
+
+card.addEventListener("mouseenter", () => {
+    cardBounds = card.getBoundingClientRect();
+    document.addEventListener("mousemove", rotateCard);
+});
+
+card.addEventListener("mouseleave", () => {
+    document.removeEventListener("mousemove", rotateCard);
+    card.style.transform = "";
+    card.style.background = "";
+});
